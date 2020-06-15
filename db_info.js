e8f91b0466924164765d90ed8136f57996e9e927
consolekakao <console@kakao.com>
2020-06-15 14:33:40 +0900

    db

---
+++ db_info.js
@@ -0,0 +1,11 @@
+module.exports = (function () {
+  return {
+    real: {
+      host: "http://alpacao.cafe24.com",
+      port: "3306",
+      user: "alpacao",
+      password: "alpaca16",
+      database: "alpacao",
+    },
+  };
+})();
